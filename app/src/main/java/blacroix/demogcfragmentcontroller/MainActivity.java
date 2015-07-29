257731ef72de907864df0725c55ae43144c73104
blacroix <benjlacroix@gmail.com>
2015-07-29 17:25:18 +0200

    Demo app initial commit

---
+++ app/src/main/java/blacroix/demogcfragmentcontroller/MainActivity.java
@@ -0,0 +1,31 @@
+package blacroix.demogcfragmentcontroller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        final MainFragment fragment = new MainFragment();
+        fragment.setContext(this);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+    }
+
+    @OnClick(R.id.second_activity)
+    public void SecondActivityClick() {
+        final Intent intent = new Intent(this, SecondActivity.class);
+        // Needed to be able to finish current activity
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/blacroix/demogcfragmentcontroller/MainFragment.java
@@ -0,0 +1,25 @@
+package blacroix.demogcfragmentcontroller;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+}
